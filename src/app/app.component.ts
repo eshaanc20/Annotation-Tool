804b026f8abd6a8508e49d75db9f1432f7c669bc
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-02-21 18:49:57 -0500

    Reading input file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   image: string = null;
+  uploadImage = (event) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = (e) => {
+        let newImage = new Image();
+        this.image = fileReader.result as string;
+        console.log(this.image);
+      };
+    }
+  }
+
 }
